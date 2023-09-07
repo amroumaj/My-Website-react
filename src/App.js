81642980cebb29774a5333d9ad55d61f973884a0
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-09-07 20:26:19 +0100

    added Router v6 to navbar with other minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import Register from "./Register";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<about />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+       
+        <Footer />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,25 @@
-//import {link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = ()=>{
     return(
-        <nav className="navbar">
-            <h1>My website</h1>
-            <div className="nav-list">
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/Sign-up">Sign up</a>
-                    <a href="/login">Login</a>
-            </div>
-        </nav>
-    );
+      <nav className="navbar">
+      <h1>My website</h1>
+      <div className="nav-list">
+            <Link to ="/">Home</Link>
+            <Link to ="/about">About</Link>
+            <Link to ="/Register">Sign up</Link>
+            <Link to ="/Login">Login</Link>
+      </div>
+      </nav>
+/*     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<about />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router> */
+  );
 }
 export default Navbar;
