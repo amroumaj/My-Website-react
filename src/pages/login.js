8959610925513b022818e5a1cd22a84d92f81a85
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-09-08 22:32:32 +0100

    added the Sign in page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import {useRef, useState, useEffect} from 'react';
 
 const Login = () => {
+  const userRef =useRef();
+  const errRef =useRef();
+  
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] =useState('');
+  const [errMsg, setErrMsg] =useState('');
+  const [success, setSuccess] = useState(false);
+  
+  useEffect(()=>{
+    userRef.current.focus();
+  },[])
+
+  useEffect(()=>{
+    setErrMsg('');
+  },[user, pwd])
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSuccess(true);
+  }
+  
   return (
-    <div>Login</div>
+    <section>
+      <p ref = {errRef} className={errMsg ? "errmsg": "offscreen"} aria-live="assertive">{errMsg}</p>
+      <h1>Sign In</h1>
+      <form on onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username</label>
+        <input 
+          type = "text"
+          id = 'username'
+          ref = {userRef}
+          autoComplete ='off'
+          onChange = {(e)=>setUser(e.target.value)}
+          value = {user}
+          required
+        />
+        <label htmlFor='password'>Password</label>
+        <input 
+          type = "password"
+          id = 'password'
+          onChange = {(e)=>setPwd(e.target.value)}
+          value = {pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+      <p>
+        Need an account ? <br/>
+        <span className='line'>
+          <a href='#'>Sign Up</a>
+        </span>
+      </p>
+    </section>
   )
 }
 
