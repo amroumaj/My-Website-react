cd16330e06d617df9cc25e5a7144ded11fc7a86e
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-09-19 23:28:31 +0100

    some work on server.js

--- src/server.js
+++ src/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const path = require ('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 3500; 
+
 application.use('/register', require('./routes/register'));
 application.use('/auth', require('./routes/auth'));
