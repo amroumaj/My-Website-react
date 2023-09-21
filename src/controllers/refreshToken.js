d509bdeb80c740449b784ee31e5ac16e0c8dec50
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-09-21 23:41:22 +0100

    added a refresh JWT

---
+++ src/controllers/refreshToken.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const handleRefreshToken = (req, res) =>{
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = /* dataBase */.users.find(person =>person.refreshToken ==refreshToken);
+    if (!foundUser) return res.sendStatus(403);
+    //evaluating the JWT
+    jwt.verify(
+        refreshToken,process.env.REFRESH_TOKEN__SECRET,(err, decoded)=> {
+            if (err || foundUser.username !==decoded.username) return res.sendStatus(403);
+            const accessToken = jwt.sign(
+                {"username": decoded.username}, process.env,ACCESS_TOKEN_SECRET, {expiresIn: '30s' }
+            );
+            res.json({accessToken})
+        }
+    )
+}
+
+module.exports = {handleRefreshToken}
