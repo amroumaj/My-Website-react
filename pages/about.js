4b037fc452d598fb81a37a4490d57e4c4a764568
amroumaj <amrou.majdoub@etudiant-enit.utm.tn>
2023-10-06 23:44:50 +0100

    f

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+
+const about = ()=> {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum consequuntur, ratione atque cum numquam, voluptatibus pariatur earum beatae rem odio aliquid! Ut quos molestias quod sed quo, cumque distinctio!</div>
+  );
+}
+
+export default about;
